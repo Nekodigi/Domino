7f8271c05ece4a53298c961454ec9278ef374b0d
Nekodigi <uekaz@outlook.jp>
2020-08-29 11:19:14 +0900

    init

---
+++ Assets/Scripts/ImageDominoSetup.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageDominoSetup : MonoBehaviour
+{
+    public GameObject dominoObj;
+    public Texture2D targetImg;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Color[] pixels = targetImg.GetPixels();
+        int iwidth = targetImg.width;
+        int iheight = targetImg.height;Debug.Log(iwidth +""+ iheight);
+        Camera.main.transform.position = new Vector3(iwidth/2*1.1f, iheight/2f*1.1f*Mathf.Sqrt(3)+2, iheight/2*1.1f);
+        for (int i = 0; i < iwidth; i++)
+        {
+            for (int j = 0; j < iheight; j++)
+            {
+                Color pixel = pixels[i + j * iwidth];
+                GameObject obj = Instantiate(dominoObj, new Vector3(i*1.1f, 1, j*1.1f), Quaternion.identity) as GameObject;
+                obj.GetComponent<Renderer>().material.color = pixel;
+                if (i == iwidth - 1) obj.transform.eulerAngles = new Vector3(0, 0, 10);//start domino
+
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/NiagaraDominoSetup.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NiagaraDominoSetup : MonoBehaviour
+{
+    public int nI;
+    public int nJ;
+    public GameObject kaplaObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float xspace = 0.36f;//x spacing
+        float zspace = 0.14f;//z spacing
+        Camera.main.transform.position = new Vector3(xspace*nI/2f, 0.03f*nJ, -xspace * nI / 4f * Mathf.Sqrt(3));
+        for (int j = 0; j < nJ; j++)
+        {
+            for (int i = 0; i < nI; i++)
+            {
+                float ry = 90;
+                float px = i * xspace;
+                float pz = 0;
+                if (j % 2 == 0)
+                {
+                    ry = 20 - i % 2 * 40;
+                }
+                else
+                {
+                    if (i == nI - 1) continue;
+                    px += xspace/2;
+                    pz = zspace - zspace*2 * (i % 2);
+                    if (i == 0)
+                    {
+                        Instantiate(kaplaObj, new Vector3(-0.03f, 0.03f + j * 0.06f, -0.0823f), Quaternion.Euler(0, 20, 0));
+                    }
+                    if (i == nI-2)
+                    {
+                        Instantiate(kaplaObj, new Vector3((i+1) * xspace + 0.03f, 0.03f + j * 0.06f, -0.0823f), Quaternion.Euler(0, -20, 0));
+                    }
+                }
+                Quaternion angle = Quaternion.Euler(0, ry, 0);
+                Instantiate(kaplaObj, new Vector3(px, 0.03f + j * 0.06f, pz), angle);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
